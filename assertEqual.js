81ca14347d2b3587693cca6b6271570115eb989c
Faranak Safavi <faranak_safavi@yahoo.com>
2021-11-22 21:25:18 -0600

    assertEqual code

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  let msg1 = ["\uF44E", "failed", "!=="]
+  let msg2 = ["\uF44C", "passed", "==="];
+  let msg;
+  if (actual === expected) {
+    msg = [...msg2];
+  } else {
+    msg = [...msg1];
+  }
+  console.log(`${msg[0]} Assertion ${msg[1]}: ${actual} ${msg[2]} ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
