4c6575312d7a0b011662368f727593c238dc8ba0
Faranak Safavi <faranak_safavi@yahoo.com>
2021-11-23 14:34:06 -0600

    change in import

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-assertEqual = require('./assertEqual.js').default
+assertEqual = require('./assertEqual.js')
 const head = (myList) => myList[0];
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
